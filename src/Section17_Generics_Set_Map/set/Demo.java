810d25b23054e624f814de87184af3d93acde889
Carla Lira <carlaliradev@gmai.com>
2025-12-28 20:24:49 -0300

    Add Set examples using HashSet and TreeSet

---
+++ src/Section17_Generics_Set_Map/set/Demo.java
@@ -0,0 +1,27 @@
+package Section17_Generics_Set_Map.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        Set<String> stringSet = new HashSet<>();
+
+        stringSet.add("Anna");
+        stringSet.add("Joe");
+        stringSet.add("Julia");
+        stringSet.add("Brenda");
+
+        System.out.println(stringSet.contains("Joe"));
+
+        // HashSet does not maintain insertion order
+        for (String s : stringSet){
+            System.out.println(s);
+        }
+
+        stringSet.remove("Anna");
+        stringSet.removeIf(x -> x.length() < 4);
+        stringSet.removeIf(x -> x.charAt(0) == 'J');
+    }
+}
---
+++ src/Section17_Generics_Set_Map/set/SecondDemo.java
@@ -0,0 +1,31 @@
+package Section17_Generics_Set_Map.set;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SecondDemo {
+    public static void main(String[] args) {
+
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
+
+        //union
+        Set<Integer> c = new TreeSet<>(a);
+        c.addAll(b);
+        System.out.println("Arrays a and b united");
+        System.out.println(c);
+
+        //intersection
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println("Keep all the elements in common between arrays a and b");
+        System.out.println(d);
+
+        //difference
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println("Keep all the elements present only in the array a");
+        System.out.println(e);
+    }
+}
