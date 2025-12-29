c9fde3d316af3988c894266e10fbcf697410b8b0
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2025-12-28 22:15:38 -0300

    Add resolved exercise to count unique users from log file using HashSet

---
+++ src/Section17_Generics_Set_Map/set/resolved_exercise/application/Program.java
@@ -0,0 +1,42 @@
+package Section17_Generics_Set_Map.set.resolved_exercise.application;
+
+import Section17_Generics_Set_Map.set.resolved_exercise.entities.LogEntry;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter file full path: ");
+        String path = scanner.nextLine();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))){
+
+            Set<LogEntry> set = new HashSet<>();
+
+            String line = bufferedReader.readLine();
+
+            while (line != null){
+                String[] fields = line.split(" ");
+                String username = fields[0];
+                Date moment = Date.from(Instant.parse(fields[1]));
+                set.add(new LogEntry(username, moment));
+                line = bufferedReader.readLine();
+            }
+            System.out.println("Total users: " + set.size());
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        scanner.close();
+    }
+}
