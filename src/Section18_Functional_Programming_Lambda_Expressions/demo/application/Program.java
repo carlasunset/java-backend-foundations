5eefc75279ca65cfd13c80661b81fc3e7d2093b8
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2025-12-30 17:19:26 -0300

    Move product sorting logic to external Comparator

--- src/Section18_Functional_Programming_Lambda_Expressions/demo/application/Program.java
+++ src/Section18_Functional_Programming_Lambda_Expressions/demo/application/Program.java
@@ -4,6 +4,7 @@ import Section18_Functional_Programming_Lambda_Expressions.demo.entities.Product
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -14,7 +15,7 @@ public class Program {
         productList.add(new Product("Laptop", 1200.00));
         productList.add(new Product("Tablet", 450.00));
 
-        Collections.sort(productList);
+        productList.sort(new MyComparator());
 
         for(Product p : productList){
             System.out.println(p);
--- src/Section18_Functional_Programming_Lambda_Expressions/demo/entities/Product.java
+++ src/Section18_Functional_Programming_Lambda_Expressions/demo/entities/Product.java
@@ -1,6 +1,6 @@
 package Section18_Functional_Programming_Lambda_Expressions.demo.entities;
 
-public class Product implements Comparable<Product> {
+public class Product {
     private String name;
     private Double price;
 
@@ -28,10 +28,5 @@ public class Product implements Comparable<Product> {
     @Override
     public String toString() {
         return "Product{" + "name='" + name + '\'' + ", price=" + price + '}';
-    }
-
-    @Override
-    public int compareTo(Product p) {
-        return name.toUpperCase().compareTo(p.getName().toUpperCase());
     }
 }
