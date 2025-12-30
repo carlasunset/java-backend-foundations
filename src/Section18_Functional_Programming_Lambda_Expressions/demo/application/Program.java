0ab078e2500e81eb771ce4599f16cacae770c1d9
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2025-12-30 17:36:09 -0300

    Simplify lambda expression for product sorting

--- src/Section18_Functional_Programming_Lambda_Expressions/demo/application/Program.java
+++ src/Section18_Functional_Programming_Lambda_Expressions/demo/application/Program.java
@@ -3,7 +3,6 @@ package Section18_Functional_Programming_Lambda_Expressions.demo.application;
 import Section18_Functional_Programming_Lambda_Expressions.demo.entities.Product;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -15,9 +14,7 @@ public class Program {
         productList.add(new Product("Laptop", 1200.00));
         productList.add(new Product("Tablet", 450.00));
 
-        Comparator<Product> comparator = (p1, p2) -> {
-            return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-        };
+        Comparator<Product> comparator = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 
         productList.sort(comparator);
 
