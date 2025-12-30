e152a8abd28b90bfbeb5ec10b905e2ace913256d
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2025-12-30 17:38:19 -0300

    Inline lambda expression in list sorting

--- src/Section18_Functional_Programming_Lambda_Expressions/demo/application/Program.java
+++ src/Section18_Functional_Programming_Lambda_Expressions/demo/application/Program.java
@@ -14,9 +14,7 @@ public class Program {
         productList.add(new Product("Laptop", 1200.00));
         productList.add(new Product("Tablet", 450.00));
 
-        Comparator<Product> comparator = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-
-        productList.sort(comparator);
+        productList.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 
         for(Product p : productList){
             System.out.println(p);
