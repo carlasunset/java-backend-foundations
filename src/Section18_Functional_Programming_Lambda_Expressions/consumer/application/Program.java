6924e7718d5d3952629dff2933da6c9e2f7bcf5e
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-01 12:07:11 -0300

    Inline Consumer lambda in forEach

--- src/Section18_Functional_Programming_Lambda_Expressions/consumer/application/Program.java
+++ src/Section18_Functional_Programming_Lambda_Expressions/consumer/application/Program.java
@@ -18,11 +18,7 @@ public class Program {
 
         double factor = 1.1;
 
-        Consumer<Product> consumer = product -> {
-            product.setPrice(product.getPrice() * factor);
-        };
-
-        list.forEach(Product::nonStaticPriceUpdate);
+        list.forEach(product -> product.setPrice(product.getPrice() * factor));
 
         list.forEach(System.out::println);
 
