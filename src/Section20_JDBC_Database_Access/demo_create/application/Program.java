ad4bd78f5fe3b3d9829edc88ca092040a5773f1e
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-06 20:42:24 -0300

    Add JDBC demo to insert data using PreparedStatement

---
+++ src/Section20_JDBC_Database_Access/demo_create/application/Program.java
@@ -0,0 +1,50 @@
+package Section20_JDBC_Database_Access.demo_create.application;
+
+import Section20_JDBC_Database_Access.demo_create.db.DB;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Program {
+    public static void main(String[] args) {
+
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try{
+            connection = DB.getConnection();
+
+            preparedStatement = connection.prepareStatement(
+                    "INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) " +
+                            "VALUES (?, ?, ?, ?, ?)");
+
+            preparedStatement.setString(1, "Carl Purple");
+            preparedStatement.setString(2, "carl@gmail.com");
+            preparedStatement.setDate(3, new Date(format.parse("22/04/1985").getTime()));
+            preparedStatement.setDouble(4, 3000.00);
+            preparedStatement.setInt(5, 4);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            System.out.println("Done! Rows Affected: " + rowsAffected);
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        catch (ParseException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(preparedStatement);
+            DB.closeConnection();
+        }
+
+    }
+}
