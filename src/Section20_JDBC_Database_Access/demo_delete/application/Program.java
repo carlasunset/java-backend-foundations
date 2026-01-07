e8eeb368f594cad455fd934b591fe173e7b682d8
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-06 21:36:22 -0300

    Add JDBC demo to delete data with integrity exception handling

---
+++ src/Section20_JDBC_Database_Access/demo_delete/application/Program.java
@@ -0,0 +1,36 @@
+package Section20_JDBC_Database_Access.demo_delete.application;
+
+import Section20_JDBC_Database_Access.demo_delete.db.DbIntegrityException;
+import Section20_JDBC_Database_Access.demo_update.db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = DB.getConnection();
+
+            preparedStatement = connection.prepareStatement(
+                    "DELETE FROM department " +
+                            "WHERE Id = ?");
+
+            preparedStatement.setInt(1, 5);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            System.out.println("Done! Rows Affected: " + rowsAffected);
+        }
+        catch (SQLException e){
+           throw new DbIntegrityException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(preparedStatement);
+            DB.closeConnection();
+        }
+    }
+}
---
+++ src/Section20_JDBC_Database_Access/demo_delete/db/DbIntegrityException.java
@@ -0,0 +1,9 @@
+package Section20_JDBC_Database_Access.demo_delete.db;
+
+public class DbIntegrityException extends RuntimeException {
+
+    public DbIntegrityException(String msg){
+        super(msg);
+    }
+
+}
